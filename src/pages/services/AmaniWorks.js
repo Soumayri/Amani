b22a87be8dc6abdc34e07243458d4d8a9b16d525
soumayri <studsoum@gmail.com>
2026-03-16 04:52:30 +0100

    fix Hammer appelé mais pas utilisé

--- src/pages/services/AmaniWorks.js
+++ src/pages/services/AmaniWorks.js
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import {
-  FaHammer,
+ 
   FaDraftingCompass,
   FaCamera,
   FaClipboardCheck,
